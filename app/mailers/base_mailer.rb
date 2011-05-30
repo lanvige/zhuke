e16a085c8686947783659ec2de8c83612ee368d5
Lanvige Jiang <lanvige@gmail.com>
2011-05-31 00:40:30 +0800

    Continue foces on mailer. override the devise instructions template.
    Add global setting config file.
    
    Signed-off-by: Lanvige Jiang <lanvige@gmail.com>

---
+++ app/mailers/base_mailer.rb
@@ -0,0 +1,9 @@
+require "resque"
+
+class BaseMailer < ActionMailer::Base
+  include Resque::Mailer
+  
+  default :sender => Setting.email_sender
+  helper :application,:users
+  layout "mailer"
+end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,10 @@
-class UserMailer < ActionMailer::Base
-  default :from => "zhuke.me@gmail.com"  
-  
-  def registration_confirmation(user)  
-    mail(:to => user.email, :subject => "Registered", :from => "zhuke.me@gmail.me")  
+class UserMailer < BaseMailer
+
+  def registration_confirmation(user)
+    @title = "欢迎加入[#{Setting.app_name}]"
+    
+    #https://github.com/plataformatec/devise/blob/master/app/mailers/devise/mailer.rb
+    mail(:to => user.email, :subject => @title, :from => Setting.email_sender)  
   end
+
 end
